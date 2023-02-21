ed13593d7eb9e368d0a65cf466e86dc58978d760
chemawall <jmmuro88@gmail.com>
2023-02-21 15:52:43 +0000

    primeros pasos api route

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,8 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +18,24 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.json
+
+    user = User.query.filter_by(email=data['email'], password=data['password']).first()
+
+    if user:
+        token = create_access_token(identity=user.id)
+        return jsonify({"token":token}),200
+
+    return jsonify({"message": "usuario incorrecto"}), 200
+
+@api.route('/user', methods=['GET'])
+@jwt_required()
+def get_user():
+    user_id = get_jwt_identity()
+    user = User.query.filter_by(id=user_id).first()
+
+    return jsonify(user.serialize()),200
