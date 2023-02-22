1a86530f0ea2631918a4cd6e2ef0c34758807736
chemawall <jmmuro88@gmail.com>
2023-02-22 06:00:51 +0000

    problemas fetch

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,7 +5,7 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
-import { Signup } from "./pages/signup";
+import { Login } from "./pages/login";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -27,7 +27,7 @@ const Layout = () => {
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
-                        <Route element={<Signup />} path="/signup"/>
+                        <Route element={<Login />} path="/login"/>
                     </Routes>
                     <Footer />
                 </ScrollToTop>
